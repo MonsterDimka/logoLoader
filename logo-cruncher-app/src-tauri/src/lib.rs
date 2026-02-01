07c67df30bc2210fa7f2ba5eb03a25c1bfd59734
Kapustin Dmitry <monsterdimka@yandex.ru>
2026-02-01 17:44:10 +0300

    поиграл параметрами

--- logo-cruncher-app/src-tauri/src/lib.rs
+++ logo-cruncher-app/src-tauri/src/lib.rs
@@ -1,14 +1,24 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    println!("Привет из раста!");
+
+    format!("Hello, {}!Привет из раста!", name)
+}
+
+#[tauri::command]
+fn logo_list(logos_dir: String) -> String {
+    println!("Хочу получить список файлов тут: {logos_dir}");
+    format!("Хочу получить список файлов тут: {logos_dir}")
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, logo_list])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- logo-cruncher-app/src/app/app.component.ts
+++ logo-cruncher-app/src/app/app.component.ts
@@ -1,22 +1,40 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
-import { invoke } from "@tauri-apps/api/core";
+import {Component} from "@angular/core";
+import {RouterOutlet} from "@angular/router";
+import {invoke} from "@tauri-apps/api/core";
+import {open} from '@tauri-apps/plugin-dialog';
 
 @Component({
-  selector: "app-root",
-  imports: [RouterOutlet],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.css",
+    selector: "app-root",
+    imports: [RouterOutlet],
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  greetingMessage = "";
+    greetingMessage = "";
+    dirs = "";
 
-  greet(event: SubmitEvent, name: string): void {
-    event.preventDefault();
+    greet(event: SubmitEvent, name: string): void {
+        event.preventDefault();
 
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    invoke<string>("greet", { name }).then((text) => {
-      this.greetingMessage = text;
-    });
-  }
+
+        // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+        invoke<string>("greet", {name}).then(async (text) => {
+            this.greetingMessage = text;
+
+            const file = await open({
+                multiple: false,
+                directory: true,
+            });
+
+            if (file) {
+                this.greetingMessage = this.greetingMessage + file;
+                this.dirs = file;
+            }
+            console.log(file);
+        });
+
+        invoke<string>("logo_list", {msg: this.dirs}).then(async (dirs) => {
+            this.greetingMessage = dirs;
+        });
+    }
 }
